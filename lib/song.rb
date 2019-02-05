bd02bdb9e37e1deb23d50101ff5b214b1caac0fb
Lizzie Chan <lchan217@gmail.com>
2019-02-05 17:17:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,12 +6,12 @@ class Song
   end 
   def artist=(artist)
     @artist = artist 
-    @artist.add_song(self)
+    @artist.add_song(self) #you needed help with this line
   end 
   def self.new_by_filename(file_name)
     array = file_name.split(" - ")
     new_song = self.new(array[1])
-    new_song.artist= Artist.find_or_create_by_name(array[0])
+    new_song.artist= Artist.find_or_create_by_name(array[0]) #call on this method to associate it 
     new_song
   end
 end   
